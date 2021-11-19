fd71f5c33558072fdc9effec2bef5e85f4cc7f12
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-11-19 15:23:22 -0800

    Creates user model and migration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: "./frontend/at_a_glance.jsx",
+  entry: "./frontend/peroos.jsx",
   output: {
       path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
       filename: "bundle.js"
