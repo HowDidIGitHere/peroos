e47e027ef4e83b4e0761efabc61bacbe9f2a30c4
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-11-19 15:57:16 -0800

    Recreates project as Peroos

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.5.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.3'
+gem 'rails', '~> 5.2.6'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
@@ -47,9 +47,9 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  gem 'pry-rails'
-  gem 'binding_of_caller'
   gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'pry-rails'
   gem 'annotate'
 end
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-  entry: "./frontend/peroos.jsx",
+  entry: './frontend/peroos.jsx',
   output: {
-      path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-      filename: "bundle.js"
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
         test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/env', '@babel/react']
           }
-        },
+        }
       }
     ]
   },
   devtool: 'source-map',
   resolve: {
-    extensions: ['.js', '.jsx', '*']
+    extensions: ['.js', '.jsx', '*'],
   }
 };
