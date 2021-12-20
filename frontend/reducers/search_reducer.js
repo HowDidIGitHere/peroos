bcfaec4331d76eaf03e002020d4ef928909520f3
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-12-19 19:56:42 -0800

    Creates a search result reducer

---
+++ frontend/reducers/search_reducer.js
@@ -0,0 +1,14 @@
+import { RECEIVE_SEARCH_RESULTS } from "../actions/search_actions";
+
+const searchReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_SEARCH_RESULTS:
+      return Object.assign({}, action.results);
+    default:
+      return state;
+  }
+};
+
+export default searchReducer;
