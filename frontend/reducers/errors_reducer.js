ee9b58445ca2c6b239e4154c81914b495db825e3
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-12-28 23:30:41 -0800

    Adds search error reducer to general errors reducer for state

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -3,12 +3,14 @@ import communitiesErrorsReducer from "./communities_errors_reducer";
 import sessionErrorsReducer from "./session_errors_reducer";
 import postsErrorsReducer from "./posts_errors_reducer";
 import commentsErrorsReducer from "./comments_errors_reducer";
+import searchErrorsReducer from "./search_errors_reducer";
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
   postForm: postsErrorsReducer,
   commentForm: commentsErrorsReducer,
-  communityForm: communitiesErrorsReducer
+  communityForm: communitiesErrorsReducer,
+  search: searchErrorsReducer
 });
 
 export default errorsReducer;
