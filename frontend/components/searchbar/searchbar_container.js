9367dde602bcf0a3567987982142c2a36acb2352
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-01-05 15:08:16 -0800

    Adds search result to state and respective dispatch to searchbar component container

--- frontend/components/searchbar/searchbar_container.js
+++ frontend/components/searchbar/searchbar_container.js
@@ -1,13 +1,14 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
+import { getSearchResults } from "../../actions/search_actions";
 import SearchBar from "./searchbar";
 
 const mapStateToProps = state => ({
-
+  searchResults: state.entities.searchResults
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  getSearchResults: query => dispatch(getSearchResults(query))
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SearchBar));
