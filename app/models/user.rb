a431a9d4895adaa53bdfe7d68921f19d32c8f653
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-01-06 23:33:40 -0800

    Adds user association to votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,11 @@ class User < ApplicationRecord
     foreign_key: :commenter_id,
     class_name: :Comment
 
+  has_many :votes,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :Vote
+
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     if user && user.is_valid_password?(password)
