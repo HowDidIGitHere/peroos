597516c9753d98c1bf7abac97e2044bb0cb6c99b
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-01-07 17:33:40 -0800

    Adds conditional for post vs comment, but need to figure out polymorphism for association

--- app/controllers/api/votes_controller.rb
+++ app/controllers/api/votes_controller.rb
@@ -4,7 +4,13 @@ class Api::FollowsController < ApplicationController
     @vote = Vote.new(vote_params)
     @vote.user_id = current_user.id
     if @vote.save
-      # check which page you're on
+      currPage = params[:vote][:currPage]
+      if currPage == 'post'
+        # @post = 
+      elsif currPage == 'comment'
+        # @comment = 
+      end
+      render 'api/' + currPage + 's/show'
     else
       render json: @vote.errors.full_messages, status:401
     end
@@ -13,7 +19,13 @@ class Api::FollowsController < ApplicationController
   def destroy
     @vote = Vote.find_by(user_id: current_user.id, parent_id: params[:vote][:parent_id], parent_type: params[:vote][:parent_type])
     @vote.delete
-    # check which page you're on
+    currPage = params[:vote][:parenttype] # Will say 'api/posts/show' or 'api/comments/show'
+    if currPage == 'post'
+      # @post = 
+    elsif currPage == 'comment'
+      # @comment = 
+    end
+    render 'api/' + currPage + 's/show'
   end
 
   private
