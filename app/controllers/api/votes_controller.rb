e6750e2d4929c78d861cd52536e35a548aba619f
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-01-11 21:25:04 -0800

    Refactors vote create CRUD method to allow render of correct api endpoint

--- app/controllers/api/votes_controller.rb
+++ app/controllers/api/votes_controller.rb
@@ -1,4 +1,4 @@
-class Api::FollowsController < ApplicationController
+class Api::VotesController < ApplicationController
 
   # Need to work on
   def index
@@ -10,11 +10,13 @@ class Api::FollowsController < ApplicationController
     @vote = Vote.new(vote_params)
     @vote.user_id = current_user.id
     if @vote.save
-      currPage = params[:vote][:currPage]
-      if currPage == 'post'
-        # @post = 
-      elsif currPage == 'comment'
-        # @comment = 
+      currPage = params[:vote][:parent_type]
+      if currPage == 'Post'
+        currPage = 'post'
+        @post = Post.find(params[:vote][:parent_id])
+      elsif currPage == 'Comment'
+        currPage = 'comment'
+        @comment = Comment.find(params[:vote][:parent_id])
       end
       render 'api/' + currPage + 's/show'
     else
@@ -26,10 +28,10 @@ class Api::FollowsController < ApplicationController
     @vote = Vote.find_by(user_id: current_user.id, parent_id: params[:vote][:parent_id], parent_type: params[:vote][:parent_type])
     @vote.delete
     currPage = params[:vote][:parent_type] # Will say 'api/posts/show' or 'api/comments/show'
-    if currPage == 'post'
-      # @post = 
-    elsif currPage == 'comment'
-      # @comment = 
+    if currPage == 'Post'
+      currPage = 'post'
+    elsif currPage == 'Comment'
+      currPage = 'comment'
     end
     render 'api/' + currPage + 's/show'
   end
