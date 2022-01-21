602c624d577bee74e006b4555fda6974046d35ee
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-01-20 21:24:34 -0800

    Creates highlight system for votes on individual posts as well as login redirect for signed out users

--- frontend/components/post/post.jsx
+++ frontend/components/post/post.jsx
@@ -17,7 +17,8 @@ class Post extends React.Component {
       upvotes: 0,
       downvotes: 0,
       upvoteActive: false,
-      downvoteActive: false
+      downvoteActive: false,
+      signedOut: false
     }
     this.handleCountInc = this.handleCountInc.bind(this);
     this.handleCountDec = this.handleCountDec.bind(this);
@@ -46,11 +47,45 @@ class Post extends React.Component {
   componentDidMount() {
     this.props.getAllComments();
     this.props.getCommunity();
-    this.props.getCurrentPost();
-    this.props.getCurrentUserVotes()
-    //   .then(() => {
-    //     if (this.props.currentUserVotes[''])
-    //   })
+    this.props.getCurrentPost()
+      .then(() => {
+        this.setState({
+          upvotes: this.props.currentPost.upvotes,
+          downvotes: this.props.currentPost.downvotes
+        });
+      });
+    if (this.props.currentUserId !== null) {
+      this.props.getCurrentUserVotes()
+        .then(() => {
+          if (this.props.currentUserVotes[`Post${this.props.match.params.postId}`]) {
+            if (this.props.currentUserVotes[`Post${this.props.match.params.postId}`].upvote) {
+              this.setState({ 
+                upvoteActive: true,
+                downvoteActive: false,
+                signedOut: false
+              });
+            } else {
+              this.setState({ 
+                upvoteActive: false,
+                downvoteActive: true,
+                signedOut: false
+              });
+            }
+          } else {
+            this.setState({
+              upvoteActive: false,
+              downvoteActive: false,
+              signedOut: false
+            })
+          }
+        });
+    } else {
+      this.setState({
+        upvoteActive: false,
+        downvoteActive: false,
+        signedOut: true
+      })
+    }
 
     // if (this.props.currentUserVotes)
 
@@ -103,6 +138,48 @@ class Post extends React.Component {
     this.setDownvote();
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.currentUserId !== this.props.currentUserId) {
+      if (this.props.currentUserId !== null) {
+        this.props.getCurrentUserVotes()
+        .then(() => {
+          if (this.props.currentUserVotes[`Post${this.props.match.params.postId}`]) {
+            if (this.props.currentUserVotes[`Post${this.props.match.params.postId}`].upvote) {
+              this.setState({ 
+                upvoteActive: true,
+                downvoteActive: false,
+                signedOut: false
+              });
+            } else {
+              this.setState({ 
+                upvoteActive: false,
+                downvoteActive: true,
+                signedOut: false
+              });
+            }
+          } else {
+            this.setState({
+              upvoteActive: false,
+              downvoteActive: false,
+              signedOut: false
+            })
+          }
+        });
+      } else {
+        this.setState({
+          upvoteActive: false,
+          downvoteActive: false,
+          signedOut: true
+        })
+      }
+    }
+    // if (this.props.currentUserId && prevProps.currentUserId !== this.props.currentUserId) {
+
+    // } else if (this.props.currentUserId === null) {
+
+    // }
+  }
+
   componentWillUnmount() {
     this.props.removeCurrentPost();
     // .then(() => this.setState({ count: this.props.count - 1 }))
@@ -112,6 +189,7 @@ class Post extends React.Component {
     if (!this.props.community || !this.props.currentPost) {
       return '...loading'
     }
+    console.log(this.state)
     return (
       <div className='post-con'>
         <div>
@@ -146,7 +224,13 @@ class Post extends React.Component {
                       {
                         <button
                           className={this.state.upvoteActive ? "upvote red_vote_button" : "upvote"} 
-                          onClick={() => this.handleUpvote()}
+                          onClick={() => {
+                            if (this.state.signedOut) {
+                              return this.props.openModal('login');
+                            } else {
+                              return this.handleUpvote();
+                            }
+                          }}
                         >
                           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-up" className="svg-inline--fa fa-arrow-up fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <path fill="currentColor" d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"></path>
@@ -206,7 +290,13 @@ class Post extends React.Component {
                       {
                         <button
                           className={this.state.downvoteActive ? "downvote blue_vote_button" : "downvote"} 
-                          onClick={() => this.handleDownvote()}
+                          onClick={() => {
+                            if (this.state.signedOut) {
+                              return this.props.openModal('login');
+                            } else {
+                              return this.handleDownvote();
+                            }
+                          }}
                         >
                           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down" className="svg-inline--fa fa-arrow-down fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <path fill="currentColor" d="M413.1 222.5l22.2 22.2c9.4 9.4 9.4 24.6 0 33.9L241 473c-9.4 9.4-24.6 9.4-33.9 0L12.7 278.6c-9.4-9.4-9.4-24.6 0-33.9l22.2-22.2c9.5-9.5 25-9.3 34.3.4L184 343.4V56c0-13.3 10.7-24 24-24h32c13.3 0 24 10.7 24 24v287.4l114.8-120.5c9.3-9.8 24.8-10 34.3-.4z"></path>
