d7c0f6f1e3bf46d0097fe7a0e538a72439008215
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-01-21 16:57:12 -0800

    Adds cases for single additions and removals to votes reducer

--- frontend/reducers/votes_reducer.js
+++ frontend/reducers/votes_reducer.js
@@ -1,11 +1,18 @@
-import { RECEIVE_CURRENT_USER_VOTES } from '../actions/vote_actions'
+import { RECEIVE_CURRENT_USER_VOTE, RECEIVE_CURRENT_USER_VOTES, REMOVE_CURRENT_USER_VOTE } from '../actions/vote_actions'
 
 const currentUserVotesReducer = (state = {}, action) => {
   Object.freeze(state);
+  let nextState = Object.assign({}, state);
 
   switch (action.type) {
     case RECEIVE_CURRENT_USER_VOTES:
       return action.votes;
+    case RECEIVE_CURRENT_USER_VOTE:
+      nextState[`${action.vote.parent_type}${action.vote.parent_id}`] = action.vote;
+      return nextState;
+    case REMOVE_CURRENT_USER_VOTE:
+      delete nextState[`${action.vote.parent_type}${action.vote.parent_id}`];
+      return nextState;
     default:
       return state;
   }
