3105e2b5c48a2b6cbdc3429a40664ac19cfe801e
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-01-25 12:56:03 -0800

    Adds colored community icon when viewing components on community specific page

--- frontend/components/community_page/community_post_about.jsx
+++ frontend/components/community_page/community_post_about.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CommunityPostAbout = (props) => {
+  const communityColor = {
+    color: props.community.color ? props.community.color : '#1a6dcd'
+  };
+
   return (
     <div className='about-community card aside-card'>
       <div className='about-community-header added-space-for-post-about-header'>
@@ -21,7 +25,7 @@ const CommunityPostAbout = (props) => {
                 </svg>
               </span> */}
               {/* <span className='border-circle'> */}
-                <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="product-hunt" className="sub-icon svg-inline--fa fa-product-hunt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="product-hunt" style={communityColor} className="sub-icon svg-inline--fa fa-product-hunt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                   <path fill="currentColor" d="M326.3 218.8c0 20.5-16.7 37.2-37.2 37.2h-70.3v-74.4h70.3c20.5 0 37.2 16.7 37.2 37.2zM504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zm-128.1-37.2c0-47.9-38.9-86.8-86.8-86.8H169.2v248h49.6v-74.4h70.3c47.9 0 86.8-38.9 86.8-86.8z"></path>
                 </svg>
               {/* </span> */}
--- frontend/components/post/post.jsx
+++ frontend/components/post/post.jsx
@@ -220,6 +220,9 @@ class Post extends React.Component {
 
   render() {
     if (this.props.community && this.props.currentPost) {
+      const communityColor = {
+        color: this.props.community.color ? this.props.community.color : '#1a6dcd'
+      };
       return (
         <div className='post-con'>
           <div>
@@ -304,6 +307,11 @@ class Post extends React.Component {
                       </div>
                     </div>
                     <div className='post-content-posted-by'>
+                      <span className="sub-list-icon">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="product-hunt" style={communityColor} className="svg-inline--fa fa-product-hunt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                          <path fill="currentColor" d="M326.3 218.8c0 20.5-16.7 37.2-37.2 37.2h-70.3v-74.4h70.3c20.5 0 37.2 16.7 37.2 37.2zM504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zm-128.1-37.2c0-47.9-38.9-86.8-86.8-86.8H169.2v248h49.6v-74.4h70.3c47.9 0 86.8-38.9 86.8-86.8z"></path>
+                        </svg>
+                      </span>
                       <div>
                         <Link to={`/${this.props.community.sub}`}>p/{this.props.community.sub}</Link>
                       </div>
