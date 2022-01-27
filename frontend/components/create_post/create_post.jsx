df256bbb928b0518a19001f8a2e4d5486f26587e
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-01-26 18:12:50 -0800

    Implements dropdown menu for selecting followed community to choose which one to post to

--- frontend/components/create_post/create_post.jsx
+++ frontend/components/create_post/create_post.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CommunityPostAbout from "../community_page/community_post_about";
 import FooterCard from "../sidebar/footer_card";
 import PostingToCard from "./posting_to_peroos_card";
 
@@ -13,13 +14,28 @@ class CreatePost extends React.Component {
         media: '',
         link: '',
         community_id: undefined
-      }
+      },
+      toggleCommunityChoice: false,
+      communityChoice: ''
     }
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDropdown = this.handleDropdown.bind(this);
   }
 
   componentDidMount() {
-    this.props.getCommunity().then(response => this.setState({ post: Object.assign({}, this.state.post, { community_id: response.community.id }) }));
+    this.props.getFollowedCommunities();
+    if (this.props.match.params.communityTitle) {
+      this.props.getCommunity()
+        .then(response => this.setState({ post: Object.assign({}, this.state.post, { community_id: response.community.id }) }))
+        .fail(res => console.log(res));
+    }
+  }
+
+  handleDropdown() {
+    document.getElementById('community-selection-input').focus();
+    this.setState({
+      toggleCommunityChoice: !this.state.toggleCommunityChoice
+    });
   }
 
   handleChange(type) {
@@ -53,28 +69,68 @@ class CreatePost extends React.Component {
                 </a>
                 {/* <button>DRAFTS<span>ICON</span></button> */}
               </div>
-              {/* <div className='community-selection-tab'>
+              <div className='community-selection-tab'>
                 <div>
-                  <div>
-                    <span>
-
-                    </span>
-                    <div>
-                      <input/>
+                  <form role='search'>
+                    <label>
+                      <div>
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" className="svg-inline--fa fa-search fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                          <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+                        </svg>
+                      </div>
+                    </label>
+                    <input id='community-selection-input' type='text' placeholder='Choose a community' onClick={this.handleDropdown}/>
+                    <div onClick={this.handleDropdown}>
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" className="svg-inline--fa fa-chevron-down fa-w-14 chevron" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
+                      </svg>
                     </div>
-                    <svg>
-
-                    </svg>
-                  </div>
+                  </form>
                 </div>
-              </div> */}
+              </div>
+
+              {
+                this.state.toggleCommunityChoice ? (
+                  <div className='community-selection-dropdown'>
+                    <div>
+                      {
+                        this.state.communityChoice.length > 0 ? (
+                          <div>
+                            IMPLEMENT SEARCH
+                          </div>
+                        ) : (
+                          <div>
+                            <h1>MY COMMUNITIES</h1>
+                            <ul className='my-communities'>
+                              {
+                                this.props.myCommunities.map((community, idx) => {
+                                  return <li key={`followed-community-${idx}`}>{community.sub}</li>
+                                })
+                              }
+                            </ul>
+                          </div>
+                        )
+                      }
+                      <div>
+
+                      </div>
+                    </div>
+                  </div>
+                ) : null
+              }
+
               <div className='create-post-form'>
                 <div className='create-post-form-type-tabs'>
                   <div>
-                    <button>Post</button>
-                    <button>Images & Video</button>
+                    <button>
+                      <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="file-alt" className="svg-inline--fa fa-file-alt fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                        <path fill="currentColor" d="M288 248v28c0 6.6-5.4 12-12 12H108c-6.6 0-12-5.4-12-12v-28c0-6.6 5.4-12 12-12h168c6.6 0 12 5.4 12 12zm-12 72H108c-6.6 0-12 5.4-12 12v28c0 6.6 5.4 12 12 12h168c6.6 0 12-5.4 12-12v-28c0-6.6-5.4-12-12-12zm108-188.1V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V48C0 21.5 21.5 0 48 0h204.1C264.8 0 277 5.1 286 14.1L369.9 98c9 8.9 14.1 21.2 14.1 33.9zm-128-80V128h76.1L256 51.9zM336 464V176H232c-13.3 0-24-10.7-24-24V48H48v416h288z"></path>
+                      </svg>
+                      Post
+                    </button>
+                    {/* <button>Images & Video</button>
                     <button>Link</button>
-                    <button>Poll</button>
+                    <button>Poll</button> */}
                   </div>
                 </div>
                 <div className='create-post-form-input'>
@@ -112,6 +168,9 @@ class CreatePost extends React.Component {
           </div>
           <div className='content-sidebar'>
             <div className='sidebar-cards-container'>
+              {
+                this.props.match.params.communityTitle && this.props.community ? <CommunityPostAbout follow={this.props.follow} unfollow={this.props.unfollow} openModal={this.props.openModal} match={this.props.match} community={this.props.community} currentUserId={this.props.currentUserId} /> : null
+              }
               <PostingToCard />
               <FooterCard />
             </div>
--- frontend/components/create_post/create_post_container.js
+++ frontend/components/create_post/create_post_container.js
@@ -1,22 +1,37 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import { getCommunity } from "../../actions/community_actions";
+import { followCommunity, getCommunity, getFollowedCommunities, unfollowCommunity } from "../../actions/community_actions";
 import CreatePost from "./create_post";
-import { createPost, voteOnPost } from '../../actions/post_actions'
+import { createPost } from '../../actions/post_actions'
+import { openModal } from "../../actions/modal_actions";
 
 const mapStateToProps = (state, ownProps)=> {
+  // let myCommunities = Object.values(state.entities.communities).filter((community) => {
+  //   if (community.followed_by_current_user) {
+  //     return community;
+  //   }
+  // });
+
   if (ownProps.match.params.communityTitle) {
-    return { community: state.entities.communities[ownProps.match.params.communityTitle] };
+    return {
+      community: state.entities.communities[ownProps.match.params.communityTitle],
+      myCommunities: Object.values(state.entities.communities)
+    };
   } else {
-    return {};
+    return {
+      myCommunities: Object.values(state.entities.communities)
+    };
   }
 
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
+  getFollowedCommunities: () => dispatch(getFollowedCommunities()),
   getCommunity: () => dispatch(getCommunity(ownProps.match.params.communityTitle)),
   createPost: post => dispatch(createPost(post)),
-  // voteOnPost: vote => dispatch(voteOnPost(vote))
+  follow: f => dispatch(followCommunity(f)),
+  unfollow: f => dispatch(unfollowCommunity(f)),
+  openModal: type => dispatch(openModal(type))
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CreatePost));
