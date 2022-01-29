b9d2d3f0027b2c17f0daa6ff9d121a2698ca0ba5
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-01-28 20:33:19 -0800

    Adds async for saving to clipboard

--- frontend/components/post/post.jsx
+++ frontend/components/post/post.jsx
@@ -25,6 +25,15 @@ class Post extends React.Component {
     this.handleCountDec = this.handleCountDec.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
     this.handleEdit = this.handleEdit.bind(this);
+    this.copyLinkToClipboard = this.copyLinkToClipboard.bind(this);
+  }
+
+  async copyLinkToClipboard(link) {
+    if ('clipboard' in navigator) {
+      return await navigator.clipboard.writeText(link);
+    } else {
+      return document.execCommand('copy', true, link);
+    }
   }
 
   handleEdit(e) {
@@ -350,7 +359,12 @@ class Post extends React.Component {
                           <p>{this.props.currentPost ? (isNaN(this.props.currentPost.comments_count + this.state.count) ? 0 : this.props.currentPost.comments_count + this.state.count) : 0} Comments</p>
                         </Link>
                         <div className='post-share-button'>
-                          <button>
+                          <button 
+                            onClick={() => {
+                              this.copyLinkToClipboard(window.location.href)
+                                .then(() => alert('worked'));
+                            }}
+                          >
                             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="share" className="svg-inline--fa fa-share fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                               <path fill="currentColor" d="M503.691 189.836L327.687 37.851C312.281 24.546 288 35.347 288 56.015v80.053C127.371 137.907 0 170.1 0 322.326c0 61.441 39.581 122.309 83.333 154.132 13.653 9.931 33.111-2.533 28.077-18.631C66.066 312.814 132.917 274.316 288 272.085V360c0 20.7 24.3 31.453 39.687 18.164l176.004-152c11.071-9.562 11.086-26.753 0-36.328z"></path>
                             </svg>
--- frontend/components/post_card/post_card.jsx
+++ frontend/components/post_card/post_card.jsx
@@ -53,7 +53,14 @@ class PostCard extends React.Component {
         signedOut: true
       });
     }
-
+  }
+  
+  async copyLinkToClipboard(link) {
+    if ('clipboard' in navigator) {
+      return await navigator.clipboard.writeText(link);
+    } else {
+      return document.execCommand('copy', true, link);
+    }
   }
 
   handleUpvote() {
@@ -249,7 +256,12 @@ class PostCard extends React.Component {
                   </div>
                   {/* </Link> */}
                   <div className='post-share-button'>
-                    <button>
+                    <button 
+                      onClick={() => {
+                        this.copyLinkToClipboard(window.location.href)
+                          .then(() => alert('worked'));
+                      }}
+                    >
                       <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="share" className="svg-inline--fa fa-share fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         <path fill="currentColor" d="M503.691 189.836L327.687 37.851C312.281 24.546 288 35.347 288 56.015v80.053C127.371 137.907 0 170.1 0 322.326c0 61.441 39.581 122.309 83.333 154.132 13.653 9.931 33.111-2.533 28.077-18.631C66.066 312.814 132.917 274.316 288 272.085V360c0 20.7 24.3 31.453 39.687 18.164l176.004-152c11.071-9.562 11.086-26.753 0-36.328z"></path>
                       </svg>
